8512e2a185f4af6c8e3b007f2cd2a30145d2891c
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-12-01 18:56:20 +0700

    rename header

--- application/views/components/navbar.php
+++ application/views/components/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="<?= base_url() ?>"><b>Sistem Pakar Rekomendasi Bidang Pekerjaan IT</b> </a>
+    <a class="navbar-brand" href="<?= base_url() ?>"><b style="font-family: monospace;">JobITKuy</b> </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
